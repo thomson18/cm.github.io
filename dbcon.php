0c3fb59aca3d682c8f154edce8877be6702ae4df
thomson18 <46535059+thomson18@users.noreply.github.com>
2019-02-10 20:19:56 +0530

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+	$link = mysqli_connect("localhost","sparks","intern","users");
+	if($link === false) {
+    	die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+?>
---
+++ user.php
@@ -0,0 +1,41 @@
+<?php
+    require_once('dbcon.php');
+    $query = "select * from users ";
+    $result = mysqli_query($link,$query);
+?>
+
+<html>
+	<head>
+        <link rel="stylesheet"type="text/css"href="style.css">
+        <title>Credit Management</title>
+    </head>
+
+   <body>
+        <h1>Credits</h1>
+             <table align="center" border="1px" styles="wodth:600px; line-height:40px;">
+			     <tr>
+                    <th>S.No.</th>
+    				<th>Name</th>
+    				<th>Email</th>
+    				<th>Credits</th>
+                 </tr>     
+       
+     <?php
+        while($row = myslqi_fetch_assoc($result)) {   
+     ?>        
+                    <tr>
+                        <td><?php echo $row['SNo']; ?></td>
+                        <td><?php echo $row['Name']; ?></td>
+                        <td><?php echo $row['Email']; ?></td>
+                        <td><?php echo $row['Credits']; ?></td>
+                    </tr>
+     <?php
+        }   
+      ?>
+            </table>  
+       <br><br>
+       <form>
+           <input class="MyButton" onclick="window.location.href='transfer.php'" type="button" value="Transfer" />
+       </form>
+    </body>
+</html>
